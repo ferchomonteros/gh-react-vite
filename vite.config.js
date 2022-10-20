ff816a588952407d1001b1f7037625cc915036b1
Fernando Monteros <fernando.monteros@agavesoft.com.mx>
2022-10-19 21:07:33 -0600

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://ferchomonteros.github.io/',
+  base: 'https://ferchomonteros.github.io/gh-react-vite/',
   plugins: [react()],
 })
